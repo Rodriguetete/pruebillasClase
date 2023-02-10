2ba6521ce9e83960b57435f0dd29a1461e98dc40
Rodriguetete <rodrigamer2409@gmail.com>
2023-02-10 10:35:08 +0100

    Commit

---
+++ coches/src/main/java/com/mycompany/coches/Coches.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.coches;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Coches {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a RentingtCar!");
+        System.out.println("¿Para que necesita el coche?");
+        
+        
+    }
+    
+}
